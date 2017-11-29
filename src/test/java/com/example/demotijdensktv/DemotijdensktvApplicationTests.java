d12aafff774fac9540febc85bc8be823ac3a72d3
FelixvL <digitaleservice@gmail.com>
2017-11-29 18:26:42 +0100

    test erbij test passert

--- src/test/java/com/example/demotijdensktv/DemotijdensktvApplicationTests.java
+++ src/test/java/com/example/demotijdensktv/DemotijdensktvApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import junit.framework.Assert;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemotijdensktvApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertEquals(1, 1);
 	}
 
 }
